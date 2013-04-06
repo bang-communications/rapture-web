7b2727641ca0f339df41c84e25a117ba8b9bb8cd
Jon Pretty <jon@bang-on.net>
2013-04-06 13:10:10 +0100

    Added missing source files

---
+++ src/extractors.scala
@@ -0,0 +1,58 @@
+package rapture.web
+
+import org.osgi.framework._
+import org.osgi.util.tracker._
+import scala.xml._
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.ListMap
+
+import javax.servlet.http._
+
+import rapture.io._
+
+import rapture.html._
+
+trait RequestExtractors { this: HttpServer =>
+
+  /** A standard implementaiton of a response which confirms cross-domain access corntrol */
+  def accessControlAllowOrigin(domain: String)(implicit enc: Encodings.Encoding): Response =
+    StreamResponse(200, ("Access-Control-Allow-Origin" -> domain) :: ("Access-Control-Allow-Credentials" -> "true") :: Response.NoCache, MimeTypes.`application/xml`, v => ())(enc)
+
+  /** Method for creating new HTTP header extractors for requests */
+  def withHttpHeader(h: String) = new HttpHeader(h)
+
+  class HasParam(p: Symbol) {
+    def unapply(r: WebRequest): Option[Boolean] = Some(r.exists(p))
+  }
+
+  class GetParam(p: Symbol) { def unapply(r: WebRequest): Option[String] = r.param(p) }
+
+  class GetCookie(p: Symbol) {
+    def unapply(r: WebRequest): Option[String] = r.cookie(p)
+  }
+  
+  class HasCookie(p: Symbol) {
+    def unapply(r: WebRequest): Option[Boolean] =
+      Some(r.cookie(p).isDefined)
+  }
+
+  /** Method for producing new cookie extractors for requests */
+  def getCookie(c: Symbol) = new GetCookie(c)
+  def hasCookie(c: Symbol) = new HasCookie(c)
+
+  object AsInt { def unapply(s: String): Option[Int] = try Some(s.toInt) catch { case e: Exception => None } }
+
+  /** Extract the path from the request */
+  object Path { def unapply(r: WebRequest): Option[SimplePath] = Some(r.path) }
+
+  /** Defines a pattern matching construct to be used to chain together constraints on requests */
+  object & { def unapply(r: WebRequest) = Some((r, r)) }
+
+  /** Method for creating new parameter extractors for requests */
+  def getParam(p: Symbol) = new GetParam(p)
+  def hasParam(p: Symbol) = new HasParam(p)
+  
+  class HttpHeader(p: String) { def unapply(r: WebRequest): Option[String] = r.headers.get(p).flatMap(_.headOption) }
+
+}
+
---
+++ src/handlers.scala
@@ -0,0 +1,99 @@
+package rapture.web
+
+import org.osgi.framework._
+import org.osgi.util.tracker._
+import scala.xml._
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.ListMap
+
+import javax.servlet.http._
+
+import rapture.io._
+
+import rapture.html._
+
+
+trait LpRequestHandlers { this: HttpServer =>
+  implicit val linkRedirectHandler = new Handler[Link] {
+    def response(link: Link) = RedirectResponse(Nil, link.toString)
+  }
+}
+
+trait RequestHandlers extends LpRequestHandlers { this: HttpServer =>
+
+  trait Handler[-T] { def response(t: T): Response }
+  
+  implicit def charInputHandler(implicit enc: Encodings.Encoding) = new Handler[Input[Char]] {
+    def response(in: Input[Char]) = StreamResponse(200, Response.NoCache,
+        MimeTypes.`text/plain`, { os =>
+      in > os
+      os.close()
+    })
+  }
+
+  implicit val StringInputHandler =
+    new Handler[Input[String]] {
+      implicit val enc = Encodings.`UTF-8`
+      def response(in: Input[String]) = StreamResponse(200, Response.NoCache,
+          MimeTypes.`text/plain`, { os =>
+        var ln = in.read()
+        while(ln != None) {
+          (ln+"\n").input > os
+          ln = in.read()
+        }
+        os.close()
+      })
+    }
+
+  implicit def xmlHandler(implicit enc: Encodings.Encoding) = new Handler[Seq[Node]] {
+    def response(t: Seq[Node]) = StreamResponse(200, Response.NoCache,
+        MimeTypes.`application/xml`, { os =>
+      ("<?xml version=\"1.0\" encoding=\""+enc.name+"\"?>\n").input > os
+      t.toString.input > os
+      os.close()
+    })
+  }
+  
+  implicit def csvHandler(implicit enc: Encodings.Encoding) = new Handler[Csv] {
+    def response(csv: Csv) = StreamResponse(200, List("Pragma" -> "public",
+        "Content-Disposition" -> "attachment;filename=data.csv",
+        "Expires" -> "Sat, 6 May 1995 12:00:00 GMT",
+        "Cache-Control" -> "no-store, no-cache, must-revalidate, post-check=0, pre-check=0"
+        ),
+        MimeTypes.`text/csv`, { os =>
+      csv.toString.input > os
+      os.close()
+    })
+  }
+  
+  implicit def stringHandler(implicit enc: Encodings.Encoding) = new Handler[String] {
+    def response(t: String) = StreamResponse(200, Response.NoCache,
+        MimeTypes.`text/plain`, { os =>
+      t.input > os
+      os.close()
+    })
+  }
+  
+  implicit def fileHandler = new Handler[FileUrl] {
+    def response(file: FileUrl) = FileResponse(200, Response.NoCache,
+        file.extension.toList.flatMap(MimeTypes.extension).headOption.getOrElse(
+	      MimeTypes.`text/plain`), file)
+  }
+
+  implicit val nullHandler = new Handler[Response] { def response(r: Response) = r }
+
+  implicit def jsonHandler(implicit enc: Encodings.Encoding) = new Handler[Json] {
+    def response(t: Json) = StreamResponse(200, Response.NoCache,
+        MimeTypes.`application/json`, { os =>
+      t.toString.input > os
+      os.close()
+    })
+  }
+
+  implicit def pageHandler(implicit enc: Encodings.Encoding) = new Handler[Layout.Page] {
+    def response(page: Layout.Page) = StreamResponse(page.httpStatus, Response.NoCache,
+        MimeTypes.`text/html`, { os =>
+      page.stream > os
+    })
+  }
+}
